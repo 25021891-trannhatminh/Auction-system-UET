a3ac4d9f0f20abe88f81831ff9018a9927526a26
Tran Nhat Minh <nhatminhtran0130@gmail.com>
2026-05-14 11:45:14 +0700

    Change ServerDiscovery, ServerFinder: Refactor BroadCast

--- client/src/main/java/client/service/ServerFinder.java
+++ client/src/main/java/client/service/ServerFinder.java
@@ -3,19 +3,17 @@ package client.service;
 import java.net.*;
 
 public class ServerFinder {
-    private static final String MULTICAST_GROUP = "239.1.2.3";
-    private static final int MULTICAST_PORT = 9999;
+    private static final int CONNECT_PORT = 9999;
     private static final int TIMEOUT_MS = 5000;
     public static String find() {
-        try (MulticastSocket multicast = new MulticastSocket(MULTICAST_PORT)){
-            InetAddress group = InetAddress.getByName(MULTICAST_GROUP);
-            multicast.joinGroup(group);
-            multicast.setSoTimeout(TIMEOUT_MS); // chờ tối đa 5 giây nếu không nhận được Data throw SocketTimeoutException
-
+        try {
+            DatagramSocket udp = new DatagramSocket(CONNECT_PORT);
+            udp.setSoTimeout(TIMEOUT_MS); // chờ tối đa 5 giây nếu không có Data từ Server throw Exception
             DatagramPacket packet = new DatagramPacket(new byte[256], 256);
-            multicast.receive(packet);
-            String serverIp = packet.getAddress().getHostAddress();
-            return serverIp;
+            udp.receive(packet);
+            String serverIP = packet.getAddress().getHostAddress();
+            udp.close();
+            return serverIP;
         } catch (Exception e) {
             System.out.println("Không tìm thấy server-> dùng localhost");
             return "localhost"; // fallback nếu không tìm thấy
--- server/src/main/java/server/network/ServerDiscovery.java
+++ server/src/main/java/server/network/ServerDiscovery.java
@@ -3,22 +3,21 @@ package server.network;
 import java.net.*;
 
 public class ServerDiscovery {
-    private static final String MULTICAST_GROUP = "239.1.2.3";  // Địa chỉ multicast
-    private static final int MULTICAST_PORT = 9999;
+    private static final int CONNECT_PORT = 9999;
     private static final int INTERVAL_MS = 2000;
-
     public static void start() {
         new Thread(() -> {
-            try (MulticastSocket multicast = new MulticastSocket()){
-                InetAddress groupIP = InetAddress.getByName(MULTICAST_GROUP);
-
+            try {
+                DatagramSocket udp = new DatagramSocket();
+                InetAddress connectIP = InetAddress.getByName("255.255.255.255");
+                udp.setBroadcast(true);
                 byte[] data = "AUCTION_SERVER:6666".getBytes();
                 while (true) {
                     DatagramPacket packet = new DatagramPacket(
-                        data, data.length,
-                        groupIP, MULTICAST_PORT
+                        data, data.length, connectIP
+                        , CONNECT_PORT
                     );
-                    multicast.send(packet);
+                    udp.send(packet);
                     Thread.sleep(INTERVAL_MS);
                 }
             } catch (Exception e) { e.printStackTrace(); }
