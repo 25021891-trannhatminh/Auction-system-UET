550f9e1ed4769b7f0898c94cad86833ac07b9a77
DongThiTraMy <tramydong710@gmail.com>
2026-05-28 00:37:38 +0700

    Refactor: Tách luồng xử lý thanh toán sang PaymentCommandHandler độc lập

--- server/src/main/java/server/handler/PaymentCommandHandler.java
+++ server/src/main/java/server/handler/PaymentCommandHandler.java
@@ -1,6 +1,7 @@
 package server.handler;
 
 import java.util.Arrays;
+import server.network.ClientManager;
 import server.service.PaymentService;
 
 /**
@@ -32,14 +33,29 @@ public class PaymentCommandHandler {
         try {
             int auctionId = Integer.parseInt(request[1]);
             String itemName = request.length > 2
-                    ? String.join(" ", Arrays.copyOfRange(request, 2, request.length))
+                    ? clientHandler.commandSafeText(String.join(" ", Arrays.copyOfRange(request, 2, request.length)))
                     : "Auction #" + auctionId;
 
+            // 1. GỌI HÀM CHECK QUYỀN (Hàm này chứa SQL nằm bên ClientHandler)
+            ClientHandler.PaymentAuthorization authorization = clientHandler.resolvePaymentAuthorization(auctionId);
+            if (!authorization.allowed()) {
+                clientHandler.send("PAYMENT_FAIL " + auctionId + " " + authorization.reason());
+                return;
+            }
+
+            // 2. GỌI NGHIỆP VỤ SERVICE
             boolean paymentSuccess = paymentService.processPayment(auctionId, itemName);
+
             if (paymentSuccess) {
                 clientHandler.send("PAYMENT_SUCCESS " + auctionId);
+
+                // 3. THÊM LỆNH BROADCAST ĐỂ UPDATE GIAO DIỆN CLIENT THỜI GIAN THỰC
+                ClientManager.broadcast("USER_TRANSACTIONS_DIRTY");
+                ClientManager.broadcast("USER_AUCTIONS_DIRTY");
             } else {
-                clientHandler.send("PAYMENT_FAIL " + auctionId);
+                // 4. THÊM LỆNH TRẢ VỀ LỖI CHI TIẾT (Hàm này chứa SQL nằm bên ClientHandler)
+                clientHandler.send("PAYMENT_FAIL " + auctionId + " " + clientHandler.resolvePaymentCompletionFailure(auctionId));
+                server.network.ClientManager.broadcast("USER_TRANSACTIONS_DIRTY");
             }
         } catch (NumberFormatException e) {
             clientHandler.send("PAYMENT_FAIL INVALID_FORMAT");
